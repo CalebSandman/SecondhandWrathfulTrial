26ad5f3dc34559a6b52e2a4197559365024e7d41
calebjsandman <34074345-calebjsandman@users.noreply.replit.com>
2024-05-16 20:28:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var numbers = 1234;
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("I'm Sandman " + numbers);
+  }
+}
